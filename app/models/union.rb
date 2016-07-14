8e7dfa4d969202492bcda8fc41feab6159e9b3e6
Abdullah <abd00s@users.noreply.github.com>
2016-07-14 20:18:14 +0200

    Define children() on Union

--- app/models/union.rb
+++ app/models/union.rb
@@ -2,4 +2,8 @@ class Union < ActiveRecord::Base
   has_and_belongs_to_many :people
   belongs_to :husband, :class_name => Person, :foreign_key => 'husband_id'
   belongs_to :wife, :class_name => Person, :foreign_key => 'wife_id'
+
+  def children
+    self.people - [self.husband, self.wife]
+  end
 end
