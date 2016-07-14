a9d2a2238778a4f1b0fa6469fea486a00fe4be20
Abdullah <abd00s@users.noreply.github.com>
2016-07-14 19:31:40 +0200

    Iron out seeds syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-bassam = Person.create({first_name: 'Bassam',last_name: 'Pharaon'})
-dina = Person.create({first_name: 'Dina',last_name: 'Tash'})
-nisreen = Person.create({first_name: 'Nisreen',last_name: 'Pharaon'})
-zeid = Person.create({first_name: 'Zeid',last_name: 'Shubailat'})
-gaith = Person.create({first_name: 'Gaith',last_name: 'Shubailat'}])
-union1 = Union.create({date: Date.new(1969), location: 'Amman, Jordan'},
-union2 = Union.create({date: Date.parse('2006-05-19'), location: 'Amman, Jordan'}])
-union1 << (bassam, dina, nisreen)
-union2 << (nisreen, zeid, gaith)
+Person.destroy_all
+Union.destroy_all
+bassam = Person.create(first_name: 'Bassam',last_name: 'Pharaon')
+dina = Person.create(first_name: 'Dina',last_name: 'Tash')
+nisreen = Person.create(first_name: 'Nisreen',last_name: 'Pharaon')
+zeid = Person.create(first_name: 'Zeid',last_name: 'Shubailat')
+gaith = Person.create(first_name: 'Gaith',last_name: 'Shubailat')
+union1 = Union.create(date: Date.new(1969), location: 'Amman, Jordan')
+union2 = Union.create(date: Date.parse('2006-05-19'), location: 'Amman, Jordan')
+union1.people << [bassam, dina, nisreen]
+union2.people << [nisreen, zeid, gaith]
