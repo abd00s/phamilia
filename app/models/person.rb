651616caf78ed0206ce4d600dca918451b356a3e
Abdullah <abd00s@users.noreply.github.com>
2016-07-14 21:10:02 +0200

    define parent_in() on Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,17 @@ class Person < ActiveRecord::Base
   has_and_belongs_to_many :unions
   belongs_to :father, :class_name => Person, :foreign_key => 'father_id'
   belongs_to :mother, :class_name => Person, :foreign_key => 'mother_id'
+
+  def parent_in #union(s)
+    parent_id =
+      if self.gender == 'M'
+        "husband_id"
+      else
+        "wife_id"
+      end
+
+    Union.where("#{parent_id}": self.id)
+  end
+
+  # scope :children, -> { Union.find_by() }
 end
