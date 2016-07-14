5dc9605043600221f7afc5a40f9bca4f5aa9319c
Abdullah <abd00s@users.noreply.github.com>
2016-07-14 20:12:38 +0200

    Modify seeds to include husband wife

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Person.destroy_all
 Union.destroy_all
+
 bassam = Person.create(first_name: 'Bassam',last_name: 'Pharaon')
 dina = Person.create(first_name: 'Dina',last_name: 'Tash')
 nisreen = Person.create(first_name: 'Nisreen',last_name: 'Pharaon')
 zeid = Person.create(first_name: 'Zeid',last_name: 'Shubailat')
 gaith = Person.create(first_name: 'Gaith',last_name: 'Shubailat')
-union1 = Union.create(date: Date.new(1969), location: 'Amman, Jordan')
-union2 = Union.create(date: Date.parse('2006-05-19'), location: 'Amman, Jordan')
+
+union1 = Union.create(date: Date.new(1969), location: 'Amman, Jordan', husband: bassam, wife: dina)
+union2 = Union.create(date: Date.parse('2006-05-19'), location: 'Amman, Jordan', husband: zeid, wife: nisreen)
+
 union1.people << [bassam, dina, nisreen]
 union2.people << [nisreen, zeid, gaith]
