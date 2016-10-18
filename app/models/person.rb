7c314ff1acedf11a3ecd6483fd309ff99cb0d609
Abdullah <abd00s@users.noreply.github.com>
2016-10-18 19:45:42 -0400

    Define spouse() on Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -18,4 +18,19 @@ class Person < ActiveRecord::Base
     parent_in.collect{|union| union.children}.flatten
   end
 
+  def spouse
+    return nil if parent_in.empty?
+
+    spouse_title =
+      if self.gender == 'M'
+        "wife"
+      else
+        "husband"
+      end
+
+    parent_in
+      .reject{|union| union.divorced?}
+      .first
+      .send "#{spouse_title}"
+  end
 end
