32705b05580e51026e40a80bdff3dea02c0c0e0f
Abdullah <abd00s@users.noreply.github.com>
2016-10-19 16:37:37 -0400

    Spec mother & father assocs on Person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,39 @@ RSpec.describe Person, type: :model do
   let(:nisreen) { Person.find_by(first_name: "Nisreen") } # Has all connections
   let(:dina) { Person.find_by(first_name: "Dina") } # Has no parents
   let(:gaith) { Person.find_by(first_name: "Gaith") } # Has no children, no siblings
+  let(:bassam) { Person.find_by(first_name: "Bassam") }
+
+  describe "association #father" do
+    context "person has a father in the network" do
+      it "returns the Person object of the father" do
+        expect(nisreen.father).to be_present
+        expect(nisreen.father.id).to be(bassam.id)
+      end
+    end
+
+    context "person's father is not defined in the network" do
+      it "returns nil" do
+        expect(dina.father).to_not be_present
+        expect(dina.father).to be_nil
+      end
+    end
+  end
+
+  describe "association #mother" do
+    context "person has mother in the network" do
+      it "returns the Person object of the mother" do
+        expect(nisreen.mother).to be_present
+        expect(nisreen.mother.id).to be(dina.id)
+      end
+    end
+
+    context "person's mother is not defined in the network" do
+      it "returns nil" do
+        expect(dina.mother).to_not be_present
+        expect(dina.mother).to be_nil
+      end
+    end
+  end
 
   describe "#parent_in" do
     context "person is a parent in one active marriage" do
