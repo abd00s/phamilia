2d9dec050cf75cede25b2e3e14645ed5b936446f
Abdullah <abd00s@users.noreply.github.com>
2016-10-20 14:50:17 -0400

    Associate Person to Relationship

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,9 @@ class Person < ActiveRecord::Base
   belongs_to :father, :class_name => Person, :foreign_key => 'father_id'
   belongs_to :mother, :class_name => Person, :foreign_key => 'mother_id'
 
+  has_many :root_relationships, :class_name => Relationship, foreign_key: "root_id"
+  has_many :target_relationships, :class_name => Relationship, foreign_key: "target_id"
+
   def parent_in #union(s)
     parent_id =
       if self.gender == 'M'
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,7 @@
 class Relationship < ActiveRecord::Base
   extend SetFirstOrderRelationships
   extend RelationshipBetween
+
+  belongs_to :root, :class_name => "Person"
+  belongs_to :target, :class_name => "Person"
 end
