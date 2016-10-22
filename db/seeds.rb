0e52b4527d3de2ca61ea85095b8f2aaf6a08b7b4
Abdullah <abd00s@users.noreply.github.com>
2016-10-21 21:02:57 -0400

    Add 1 generation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Person.destroy_all
 Union.destroy_all
+Relationship.destroy_all
 
+afaf = Person.create(first_name: 'Afaf',last_name: 'Tabbaa',gender: 'F')
+ar = Person.create(first_name: 'AR',last_name: 'Pharaon',gender: 'M')
 bassam = Person.create(first_name: 'Bassam',last_name: 'Pharaon',gender: 'M')
 dina = Person.create(first_name: 'Dina',last_name: 'Tash',gender: 'F')
 nisreen = Person.create(first_name: 'Nisreen',last_name: 'Pharaon',gender: 'F', father: bassam, mother: dina)
@@ -17,6 +20,8 @@ gaith = Person.create(first_name: 'Gaith',last_name: 'Shubailat',gender: 'M', fa
 
 union1 = Union.create(date: Date.new(1969), location: 'Amman, Jordan', husband: bassam, wife: dina)
 union2 = Union.create(date: Date.parse('2006-05-19'), location: 'Amman, Jordan', husband: zeid, wife: nisreen)
+union3 = Union.create(date: Date.new(1940), location: 'Amman, Jordan', husband: ar, wife: afaf)
 
 union1.people << [bassam, dina, nisreen, yasmin]
 union2.people << [nisreen, zeid, gaith]
+union3.people << [bassam, afaf, ar]
