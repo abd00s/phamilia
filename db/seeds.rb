1de5d28257e9ef40a8ad6b9ede967af135f78e98
Abdullah <abd00s@users.noreply.github.com>
2016-10-25 16:39:28 -0400

    Expand seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,17 +20,25 @@ bassam = Person.create(first_name: 'Bassam',last_name: 'Pharaon',gender: 'M', fa
 dina = Person.create(first_name: 'Dina',last_name: 'Tash',gender: 'F')
 nisreen = Person.create(first_name: 'Nisreen',last_name: 'Pharaon',gender: 'F', father: bassam, mother: dina)
 yasmin = Person.create(first_name: 'Yasmin',last_name: 'Pharaon',gender: 'F', father: bassam, mother: dina)
-zeid = Person.create(first_name: 'Zeid',last_name: 'Shubailat',gender: 'M')
+zein = Person.create(first_name: 'Zein',last_name: 'Shubailat',gender: 'F')
+gaithsr = Person.create(first_name: 'Gaith',last_name: 'Shubailat',gender: 'M')
+zeid = Person.create(first_name: 'Zeid',last_name: 'Shubailat',gender: 'M', father: gaithsr, mother: zein)
 gaith = Person.create(first_name: 'Gaith',last_name: 'Shubailat',gender: 'M', father: zeid, mother: nisreen)
+haya = Person.create(first_name: 'Haya',last_name: 'Shubailat',gender: 'F', father: gaithsr, mother: zein)
+karim = Person.create(first_name: 'Karim',last_name: 'Tabba',gender: 'M', father: tq, mother: haya)
 
 union1 = Union.create(date: Date.new(1969), location: 'Amman, Jordan', husband: bassam, wife: dina)
 union2 = Union.create(date: Date.parse('2006-05-19'), location: 'Amman, Jordan', husband: zeid, wife: nisreen)
 union3 = Union.create(date: Date.new(1940), location: 'Amman, Jordan', husband: ar, wife: afaf)
 union4 = Union.create(date: Date.new(1940), location: 'Amman, Jordan', husband: mans, wife: nadia)
 union5 = Union.create(date: Date.new(1940), location: 'Amman, Jordan', husband: hashem, wife: zahra)
+union6 = Union.create(date: Date.new(2004), location: 'Amman, Jordan', husband: tq, wife: haya)
+union7 = Union.create(date: Date.new(1960), location: 'Amman, Jordan', husband: gaithsr, wife: zein)
 
 union1.people << [bassam, dina, nisreen, yasmin]
 union2.people << [nisreen, zeid, gaith]
 union3.people << [bassam, afaf, ar]
 union4.people << [mans, nadia, tq]
 union5.people << [hashem, zahra, mans, afaf]
+union6.people << [tq, haya, karim]
+union7.people << [gaithsr, zein, zeid, haya]
